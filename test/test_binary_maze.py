43f8767238be8b246ab0f0cbb1372837f8583c12
annakozakkk <119266965+annakozakkk@users.noreply.github.com>
2023-11-02 22:36:20 +0200

    fix test

--- test/test_binary_maze.py
+++ test/test_binary_maze.py
@@ -1,9 +1,10 @@
 import unittest
 
-from src.binary_maze import bfs,Point,queueNode
+from src.binary_maze import bfs, Point, queueNode
+
 
 class MyTestCase(unittest.TestCase):
-    def find_shortest_path(self):
+    def test_find_shortest_path(self):
         adjMat = [[1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
                   [0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
                   [0, 0, 1, 0, 1, 1, 1, 0, 0, 1],
@@ -15,13 +16,12 @@ class MyTestCase(unittest.TestCase):
                   [1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
                   [0, 0, 1, 0, 0, 1, 1, 0, 0, 1]]
 
-        start =Point(0,0)
-        final =Point(7,5)
+        start = Point(0, 0)
+        final = Point(7, 5)
 
-        ROW= 10
-        COL =10
+        ROW = 10
+        COL = 10
 
-        result = bfs(adjMat,start,final)
+        result = bfs(adjMat, start, final)
 
-        self.assertEqual(result,12 )
-
+        self.assertEqual(12, result)
